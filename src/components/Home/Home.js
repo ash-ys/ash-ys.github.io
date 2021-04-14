b251a158fb10a09376a58a9b05730bc0dbd0d8be
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-04-14 18:12:17 +0545

    home added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,13 @@
+import classes from "./Home.module.css";
+import code from './../../img/code.svg'
 const Home = () => {
     return ( 
-        <div>
-            <h1>Home</h1>
+        <div className = {classes.Home}>
+            <div className= {classes.Container}>
+                <h1 className={classes.Hello}>Hello, world</h1>
+                <h1>Welcome on my website</h1>
+            </div>
+            <img className={classes.Code} src="" alt=""/>
         </div>
      );
 }  
