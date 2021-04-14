a33facb08cda452aed2959c21518ead96ff05dfb
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-04-14 18:24:26 +0545

    added images

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import classes from "./Home.module.css";
 import code from './../../img/code.svg'
+import binaryWorld from './../../img/binaryWorld.svg'
 const Home = () => {
     return ( 
         <div className = {classes.Home}>
@@ -7,8 +8,9 @@ const Home = () => {
                 <h1 className={classes.Hello}>Hello, world</h1>
                 <h1>Welcome on my website</h1>
             </div>
-            <img className={classes.Code} src="" alt=""/>
-        </div>
+            <img className={classes.Code} src={code} alt="code"></img>
+            <img className={classes.BinaryWorld} src={binaryWorld} alt="binaryWorld"></img>
+        </div> 
      );
 }  
  
