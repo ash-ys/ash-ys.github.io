5cbe6551edd00c1908d4fe4edaf93e545f382f6f
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-04-16 19:23:57 +0545

    about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,21 @@
 import classes from'./About.module.css'
 import PageHeader from './../PageHeader/PageHeader'
+import me from './../../img/me.JPG'
 const About = () => {
     return (  
-        <div>
+        <div className={classes.AboutMe}>
             <PageHeader title={'About Me'}/>
+            <div className={classes.Container}>
+                <div className={classes.Text}>
+                    <h2>Hello I'm Ashish</h2>
+                    <p>
+                        Please I need a job
+                    </p>
+                </div>
+                <div className={classes.Photo}>
+                    <img className={classes.Me} src={me} alt="me"/>
+                </div>
+            </div>
         </div>
     );
 }
