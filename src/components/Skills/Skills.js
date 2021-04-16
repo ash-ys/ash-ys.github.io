85baa14cfcff5632abab9037724de79e30cddb01
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-04-16 19:57:34 +0545

    skills added

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,7 +1,37 @@
+import classes from './Skills.module.css'
+import PageHeader from './../PageHeader/PageHeader'
+
+const backendSkills =
+<ul>
+    <li>BACKEND</li>
+    <li>Java-SQL</li>
+</ul>
+
+const frontendSkills =
+<ul>
+    <li>FRONTEND</li>
+    <li>ReactJS-HTML-CSS</li>
+</ul>
+
+const otherSkills =
+<ul>
+    <li>OTHER SKILLS</li>
+    <li>GIT-Figma</li>
+</ul>
+
 const Skills = () => {
     return ( 
         <div>
-            <h1>Skills</h1>
+            <PageHeader title={'What about the skills?'}/>
+            <p>
+                I'm a life long learner and enjoy learning new stuff!
+                Below are some programming languages ,toolkits frameworks and
+                libraries that i work with an like.
+                Some of them i control better than the others.
+            </p>
+            <div>
+                className{classes.Container}
+            </div>
         </div>
      );
 }
