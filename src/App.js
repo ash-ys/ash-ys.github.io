1cc060240a9633043fbd3297b0d5f567cb6a1af5
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-04-17 18:03:53 +0545

    contacts added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import NavBar from'./components/NavBar/NavBar'
 import Home from'./components/Home/Home'
 import About from'./components/About/About'
 import Skills from'./components/Skills/Skills'
-import Youtube from'./components/Youtube/Youtube'
 import Contact from'./components/Contact/Contact'
 const App = () => {
   return (
@@ -11,7 +10,6 @@ const App = () => {
       <Home />
       <About />
       <Skills />
-      <Youtube />
       <Contact />
     </div>
   );
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,24 @@
+import { FaYoutube, FaInstagram, FaGithub, FaLinkedin } from "react-icons/fa";
+import classes from './Contact.module.css'
+const handleURL =(url) => {
+    return () => window.open(url,"_blank")
+}
 const Contact = () => {
     return ( 
-        <div>
-            <h1>Contact</h1>
+        <div className={classes.Contact}>
+            <div className={classes.ContactIcons}>
+            <FaYoutube color ='white' size= '30px' style={{padding: '1%'}}
+            onClick={handleURL('https://www.youtube.com/channel/UC5I1ZV96xf-qffbCdFYZihg')} />
+        
+        <FaInstagram color ='white' size= '30px' style={{padding: '1%'}}
+            onClick={handleURL('https://www.instagram.com/a.s.h.i.s.h.w.a.g.l.e/')} />
+
+        <FaGithub color ='white' size= '30px' style={{padding: '1%'}}
+            onClick={handleURL('https://github.com/ash-ys')} />
+
+        <FaLinkedin color ='white' size= '30px' style={{padding: '1%'}}
+            onClick={handleURL('https://www.linkedin.com/in/ashish-wagle-a4155313a/')} />
+            </div>
         </div>
      );
 }
