fc46010f9621daca5331d015aff9abbf8d1d36dc
Ashish <53848069+ash-ys@users.noreply.github.com>
2021-04-20 19:43:29 +0545

    motion div

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,23 +9,23 @@ const Contact = () => {
         
         <div className={classes.Contact} id="contact">
             <div className={classes.ContactIcons}>
-                <div>
-            <FaYoutube color ='white' size= '30px' style={{padding: '1%'}}
-            onClick={handleURL('https://www.youtube.com/channel/UC5I1ZV96xf-qffbCdFYZihg')} />
-        </div>
-        <div>
-        <FaInstagram color ='white' size= '30px' style={{padding: '1%'}}
-            onClick={handleURL('https://www.instagram.com/a.s.h.i.s.h.w.a.g.l.e/')} />
-        </div>
-        <div>
-        <FaGithub color ='white' size= '30px' style={{padding: '1%'}}
-            onClick={handleURL('https://github.com/ash-ys')} />
-        </div>
-        <div>
-        <FaLinkedin color ='white' size= '30px' style={{padding: '1%'}}
-            onClick={handleURL('https://www.linkedin.com/in/ashish-wagle-a4155313a/')} />
-            </div>
-            </div>
+                <motion.div>
+                    <FaYoutube color ='white' size= '30px' style={{padding: '1%'}}
+                    onClick={handleURL('https://www.youtube.com/channel/UC5I1ZV96xf-qffbCdFYZihg')} />
+                </motion.div>
+                <motion.div>
+                    <FaInstagram color ='white' size= '30px' style={{padding: '1%'}}
+                    onClick={handleURL('https://www.instagram.com/a.s.h.i.s.h.w.a.g.l.e/')} />
+                </motion.div>
+                <motion.div>
+                    <FaGithub color ='white' size= '30px' style={{padding: '1%'}}
+                     onClick={handleURL('https://github.com/ash-ys')} />
+                </motion.div>
+                <motion.div>
+                    <FaLinkedin color ='white' size= '30px' style={{padding: '1%'}}
+                     onClick={handleURL('https://www.linkedin.com/in/ashish-wagle-a4155313a/')} />
+                 </motion.div>
+                 </div>
         </div>
      );
 }
